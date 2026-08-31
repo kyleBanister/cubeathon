19dfc0fa187b6ffab2cfb8ffa1bac2e72dd9e2f4
Kyle Banister <kyle@thebanisters.org>
2026-08-31 00:35:43 -0700

    Third level added.

--- Assets/Scripts/MovingObstacle.cs
+++ Assets/Scripts/MovingObstacle.cs
@@ -1,22 +1,33 @@
 using UnityEngine;
+using System;
 
 public class MovingObstacle : MonoBehaviour {
     public Rigidbody rb;
     public float pathStart; //Lower x position of path
-    public float pathLength; //Travel distance of path
-    public float frequency; //Hertz
-
+    public float pathLength = 17; //Travel distance of path
+    public float frequency = 1; //Hertz
+    private float timeValue;
     void Start() {
         if (rb.position.x < pathStart) {
-            rb.position.x = pathStart;
+            rb.position = new Vector3(pathStart,rb.position.y,rb.position.z);
         } else if(rb.position.x > pathStart+pathLength) {
-            rb.position.x = pathStart+pathLength;
+            rb.position = new Vector3(pathStart+pathLength,rb.position.y,rb.position.z);
         }
 
-        timeValue = Math.Asin(rb.position.x/pathLength)/(frequency*Math.PI); //Get where in the sine wave the obstacle is starting.
-        rb.linearVelocity.x = pathLength*frequency*Math.PI*Math.Cos(timeValue*frequency*Math.PI); //Set velocity to match sine wave position.
+        timeValue = (float)(Math.Asin(2*(rb.position.x-(pathStart+pathLength/2))/pathLength)/(frequency*Math.PI)); //Get where in the sine wave the obstacle is starting.
+        rb.linearVelocity = new Vector3((float)(pathLength*frequency*Math.PI*Math.Cos(timeValue*frequency*Math.PI)),0,0); //Set velocity to match sine wave position.
+        Debug.Log(rb.linearVelocity);
     }
     void FixedUpdate() {
-        rb.AddForce(-pathLength*Math.Pow(frequency,2)*Math.Pow(Math.PI,2)*Math.Sin(frequency*Math.PI),0,0, ForceMode.Acceleration);
+        if (rb.position.x < pathStart) {
+            rb.position = new Vector3(pathStart,rb.position.y,rb.position.z);
+        } else if(rb.position.x > pathStart+pathLength) {
+            rb.position = new Vector3(pathStart+pathLength,rb.position.y,rb.position.z);
+        }
+
+        Debug.Log((rb.position.x-pathStart)/pathLength);
+        timeValue = (float)(Math.Asin(2*(rb.position.x-(pathStart+pathLength/2))/pathLength)/(frequency*Math.PI));
+        //Debug.Log(timeValue);
+        rb.AddForce((float)(-pathLength*Math.Pow(frequency,2)*Math.Pow(Math.PI,2)*Math.Sin(frequency*Math.PI*timeValue)),0,0, ForceMode.Acceleration);
     }
 }
